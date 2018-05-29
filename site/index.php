fced131f4276c56066cbe464975bcb5f64bb0761
Evgenii <kalyashov123@mail.ru>
2018-05-29 13:49:10 +0300

    1

---
+++ site/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<?php $title = "7 чудес Череповецкого района";
+include "head.php";
+?>
+</head>
+<body bgcolor="LightBlue">
+<?php include_once "header.php"?>
+<?php include_once "menu.php"?></br>
+<?php include_once "leftblock.php"?>
+<div class="content">
+<img src="img19.jpg" height="37%" width="100%" alt=" ">
+</div>
+<?php include_once "footer.php"?>
+</body>
+</html>
---
+++ site/leftblock.php
@@ -0,0 +1,14 @@
+<div class="leftblock">
+<div id="left">
+<ul>
+<li><a href="yag.php">Ягановское озеро</a></li>
+<li><a href="Darv.php">Дарвинский заповедник</a></li>
+<li><a href="Rib.php">Рыбинское водохранилище</a></li>
+<li><a href="Leu.php">Леушинский монастырь</a></li>
+<li><a href="muz.php">Музей Игоря Северянина</a></li>
+<li><a href="Usad.php">Усадьба Хантаново</a></li>
+<li><a href="usp.php">Успенская церковь</a></li>
+</ul>
+</div>
+<a href="https://cherra.ru/" target="_blank"><img src="adm.png" height="200px" width="145px" alt=" " class="adm"/></a>
+</div>
